1dc6f7f98cccced32b19bb027cc543892f067553
philippart-s <wildagsx@gmail.com>
2024-10-21 17:59:20 +0200

    doc: 📝 add live templates for Intellij IDEA

--- python/blocking-chatbot-without-fwk.py
+++ python/blocking-chatbot-without-fwk.py
@@ -5,15 +5,18 @@ import os
 # access the environment variables from the .env file
 load_dotenv(dotenv_path='../.env')
 
+# py-02-simple-request
 url = "https://mistral-7b-instruct-v02.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1/chat/completions"
 payload = {
     "max_tokens": 512,
     "messages": [
+        # py-03-simple-sys-msg
         {
             "content": "You are Nestor, a virtual assistant. Answer to the question.",
             "name": "Nestor",
             "role": "system"
         },
+        # py-04-simple-usr-msg
         {
             "content": "What is OvHcloud?",
             "role": "user"
@@ -22,12 +25,14 @@ payload = {
     "model": "Mistral-7B-Instruct-v0.2",
     "temperature": 0,
 }
- 
+
+# py-05-simple-token
 headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {os.getenv('OVH_AI_ENDPOINTS_ACCESS_TOKEN')}",
 }
- 
+
+# py-06-simple-call
 response = requests.post(url, json=payload, headers=headers)
 if response.status_code == 200:
     # Handle response
--- talk.md
+++ talk.md
@@ -11,14 +11,14 @@
 
 ### 🛑 Blocking chatbot with no Framework 🛑
 
-- ⬇️ In the [requirements.txt](./python/requirements.txt) file, declare the dependencies: `01-simple-requirements`
+- ⬇️ In the [requirements.txt](./python/requirements.txt) file, declare the dependencies: `py-01-simple-requirements`
 - 📁 open the [blocking-chatbot-without-fwk](./python/blocking-chatbot-without-fwk.py)
 - 📝 use the snippets as written in the comments
 - ⚡️ run the script: `cd python` && `python blocking-chatbot-without-fwk.py`
 
 ### 🛑 Blocking chatbot with LangChain 🛑
 
-- ⬇️ In the [requirements.txt](./python/requirements.txt) file, declare the dependencies: `07-langchain-blk-requirements`
+- ⬇️ In the [requirements.txt](./python/requirements.txt) file, declare the dependencies: `py-07-langchain-blk-requirements`
 - 📁 open the [blocking-chatbot-langchain](./python/blocking-chatbot-langchain.py)
 - 📝 use the snippets as written in the comments
 - ⚡️ run the script: `cd python` && `python blocking-chatbot-langchain.py`
