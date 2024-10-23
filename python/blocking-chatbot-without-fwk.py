fa83904b9470292b6332a818c84b00bac630d14e
philippart-s <wildagsx@gmail.com>
2024-10-23 13:28:14 +0200

    clean: 🗑️ remove code to do demo

--- python/blocking-chatbot-without-fwk.py
+++ python/blocking-chatbot-without-fwk.py
@@ -6,42 +6,9 @@ import os
 load_dotenv(dotenv_path='../.env')
 
 # py-02-simple-request
-url = "https://mistral-7b-instruct-v02.endpoints.kepler.ai.cloud.ovh.net/api/openai_compat/v1/chat/completions"
-payload = {
-    "max_tokens": 512,
-    "messages": [
-        # py-03-simple-sys-msg
-        {
-            "content": "You are Nestor, a virtual assistant. Answer to the question.",
-            "name": "Nestor",
-            "role": "system"
-        },
-        # py-04-simple-usr-msg
-        {
-            "content": "What is OvHcloud?",
-            "role": "user"
-        }
-    ],
-    "model": "Mistral-7B-Instruct-v0.2",
-    "temperature": 0,
-}
+# py-02-simple-request
+# py-03-simple-sys-msg
+
+# py-04-simple-usr-msg
 
 # py-05-simple-token
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer {os.getenv('OVH_AI_ENDPOINTS_ACCESS_TOKEN')}",
-}
-
-# py-06-simple-call
-response = requests.post(url, json=payload, headers=headers)
-if response.status_code == 200:
-    # Handle response
-    response_data = response.json()
-    # Parse JSON response
-    choices = response_data["choices"]
-    for choice in choices:
-        text = choice["message"]["content"]
-        # Process text and finish_reason
-        print(f"🤖: {text}")
-else:
-    print("Error:", response.status_code)
