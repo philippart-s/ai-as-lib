679d87c892e2a389c6ecc3b9083a31ec81ba3f66
philippart-s <wildagsx@gmail.com>
2024-10-28 14:44:22 +0100

    clean: 🗑️ remove code

--- java-quarkus/src/main/java/fr/wilda/ai/Jarvis.java
+++ java-quarkus/src/main/java/fr/wilda/ai/Jarvis.java
@@ -1,31 +1,16 @@
 package fr.wilda.ai;
 
-import java.time.Duration;
-
-import fr.wilda.ai.service.AIEndpointService;
 import io.quarkus.websockets.next.OnTextMessage;
-import io.quarkus.websockets.next.WebSocket;
 import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.Uni;
-import jakarta.inject.Inject;
 
 // quarkus-04-websocket
-@WebSocket(path = "/jarvis")
 public class Jarvis {
 
   // quarkus-05-ws-inject-service
-  @Inject
-  AIEndpointService aiEndpointService;
 
   @OnTextMessage
   public Multi<String> onMessage(String message) {
     // quarkus-06-ws-onMessage
-    return aiEndpointService.askAQuestion(message)
-        .onItem()
-        .call(i -> Uni.createFrom()
-            .nullItem()
-            .onItem()
-            .delayIt()
-            .by(Duration.ofMillis(200)));
+    return null;
   }
 }
--- java-quarkus/src/main/java/fr/wilda/ai/service/AIEndpointService.java
+++ java-quarkus/src/main/java/fr/wilda/ai/service/AIEndpointService.java
@@ -1,13 +1,5 @@
 package fr.wilda.ai.service;
 
-import dev.langchain4j.service.SystemMessage;
-import dev.langchain4j.service.UserMessage;
-import io.quarkiverse.langchain4j.RegisterAiService;
-import io.smallrye.mutiny.Multi;
-import jakarta.enterprise.context.SessionScoped;
-
-import java.io.Serializable;
-
 // quarkus-02-ai-ai-service
 public interface AIEndpointService {
   // quarkus-03-ai-prompt
