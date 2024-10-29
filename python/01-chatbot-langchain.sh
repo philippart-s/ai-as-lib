2bc84a36a9f9caff25a2136e391c58c4ad2be12d
philippart-s <wildagsx@gmail.com>
2024-10-29 15:50:12 +0100

    merge: ⬇️ slides & scripts

---
+++ python/01-chatbot-langchain.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+bat -P -r 8: $(basename "$0")
+
+# Set some environment variables to use AI Endpoints
+source ../.env
+
+# Call the chatbot with "What is OVHcloud?"
+python blocking-chatbot-langchain.py
+
