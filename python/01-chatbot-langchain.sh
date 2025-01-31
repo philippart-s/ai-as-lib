3b8c7179a8496bf28d0428c45f212f7d16cd6f62
philippart-s <wildagsx@gmail.com>
2025-01-31 14:57:35 +0100

    enhancement: ✨ Snippets & bash

--- python/01-chatbot-langchain.sh
+++ python/01-chatbot-langchain.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-bat -P -r 8: $(basename "$0")
-
 # Set some environment variables to use AI Endpoints
 source ../.env
+
+clear
+
+bat -P -r 10: $(basename "$0")
 
 # Call the chatbot with "What is OVHcloud?"
 python blocking-chatbot-langchain.py
