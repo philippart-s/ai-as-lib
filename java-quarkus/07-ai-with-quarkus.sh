ef89599d6398ac07216c224a007178c74e429c7f
Stéphane Philippart <wildagsx@gmail.com>
2025-06-12 14:55:53 +0200

    feat: ✨ France devops 2025 rehearsal01 (#15)
    * fix: 🐛 RAG parameter too optimistic
    
    * fix: 🐛 Maven warning with JDK 24
    
    * conf: ⬆️ Upgrade dependencies
    
    * conf: ⬆️ Upgrade dependencies
    
    * conf: ⬆️ Upgrade dependencies
    
    * conf: ⬆️ Upgrade dependencies

--- java-quarkus/07-ai-with-quarkus.sh
+++ java-quarkus/07-ai-with-quarkus.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-bat -P -r 8: $(basename "$0")
+bat -P -r 11: $(basename "$0")
 
 # Set some environment variables to use AI Endpoints
 source ../.env
+
+# To deactivate warning messages from Maven su to JDK24 usage (see https://issues.apache.org/jira/browse/MNG-8760 & https://github.com/google/guice/issues/1901)
+export MAVEN_OPTS="--enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow"
 
 # Launch Quarkus in dev mode
 quarkus dev
--- java/04-chatbot-memory.sh
+++ java/04-chatbot-memory.sh
@@ -3,11 +3,14 @@
 # Set some environment variables to use AI Endpoints
 source ../.env
 
+# To deactivate warning messages from Maven su to JDK24 usage (see https://issues.apache.org/jira/browse/MNG-8760 & https://github.com/google/guice/issues/1901)
+export MAVEN_OPTS="--enable-native-access=ALL-UNNAMED --sun-misc-unsafe-memory-access=allow"
+
 # Compile before launching
 mvn clean compile
 clear
 
-bat -P -r 12: $(basename "$0")
+bat -P -r 15: $(basename "$0")
 
 # Call the chatbot with "My name is Stéphane." && "Do you remember what is my name?"
 mvn -q -e exec:java -Dexec.useMavenLogger=false -Dexec.quietLogs=true -Dexec.mainClass="fr.wilda.ai.chatbot.MemoryStreamingChatbot"
